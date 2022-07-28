0d0119de08a94bd51382a067f9d47257af3badde
Pawel Stanke <pablostanke21@gmail.com>
2022-07-28 23:21:13 +0200

    Update setValue function

--- src/js/script.js
+++ src/js/script.js
@@ -321,11 +321,11 @@
       });
       thisWidget.linkDecrease.addEventListener('click', function (event) {
         event.preventDefault();
-        thisWidget.setValue(--thisWidget.value);
+        thisWidget.setValue(thisWidget.value - 1);
       });
       thisWidget.linkIncrease.addEventListener('click', function (event) {
         event.preventDefault();
-        thisWidget.setValue(++thisWidget.value);
+        thisWidget.setValue(thisWidget.value + 1);
       });
     }
     announce() {
